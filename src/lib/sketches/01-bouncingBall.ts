4ac50ab588d011b3be036a925a12608c0a7c0b84
nonvenomous <rawlsriot@gmail.com>
2024-10-20 20:09:43 +0200

    Make use of vectors in bouncingball example

--- src/lib/sketches/01-bouncingBall.ts
+++ src/lib/sketches/01-bouncingBall.ts
@@ -10,34 +10,25 @@ export default function sketch(p: p5) {
     p.frameRate(frameRate);
   };
 
-  let x = 100;
-  let y = 100;
-  let xspeed = 2.5;
-  let yspeed = 2;
+  const position = p.createVector(100, 100);
+  const velocity = p.createVector(2.5, 2);
 
   const SIZE = 48;
   const RADIUS = SIZE / 2;
 
   p.draw = () => {
     p.background(55);
-    p.stroke(255);
-    p.line(0, canvasHeight, 0, 0);
-    p.line(canvasWidth - 1, 0, canvasWidth - 1, canvasHeight);
-    p.line(0, 0, canvasWidth, 0);
-    p.line(0, canvasHeight, canvasWidth, canvasHeight);
-
     p.stroke(55);
 
-    x = x + xspeed;
-    y = y + yspeed;
+    position.add(velocity);
 
-    if (x + RADIUS > p.width || x - RADIUS < 0) {
-      xspeed = xspeed * -1;
-    } else if (y + RADIUS > p.height || y - RADIUS < 0) {
-      yspeed = yspeed * -1;
+    if (position.x + RADIUS > p.width || position.x - RADIUS < 0) {
+      velocity.x = velocity.x * -1;
+    } else if (position.y + RADIUS > p.height || position.y - RADIUS < 0) {
+      velocity.y = velocity.y * -1;
     }
 
     p.fill(127);
-    p.circle(x, y, SIZE);
+    p.circle(position.x, position.y, SIZE);
   };
 }
