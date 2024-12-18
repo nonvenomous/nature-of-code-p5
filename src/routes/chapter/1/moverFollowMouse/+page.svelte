b19995b87047f1ef14e7897ed82093cf2b1cf653
nonvenomous <rawlsriot@gmail.com>
2024-12-18 19:49:38 +0100

    Add inverse and propotional acceleration modifiers

--- src/routes/chapter/1/moverFollowMouse/+page.svelte
+++ src/routes/chapter/1/moverFollowMouse/+page.svelte
@@ -4,7 +4,7 @@
 
   const canvasWidth = 600;
   const canvasHeight = 300;
-  const fps = 120;
+  const fps = 60;
 
   class Mover {
     position: p5.Vector;
@@ -16,20 +16,52 @@
       this.position = p.createVector(canvasWidth / 2, canvasHeight / 2);
       this.velocity = p.createVector(0, 0);
       this.acceleration = p.createVector();
-      this.topSpeed = 3;
+      this.topSpeed = 5;
+    }
+
+    private getConstantAcceleration(_p: p5, v: p5.Vector) {
+      const constantDirection = p5.Vector.sub(v, this.position);
+      constantDirection.setMag(0.03);
+      return constantDirection;
+    }
+
+    // Linear inverse relationship
+    private getInverseToDistance(p: p5, v: p5.Vector) {
+      const gravitationalDirection = p5.Vector.sub(v, this.position);
+
+      let distance = gravitationalDirection.mag();
+      distance = p.constrain(distance, 100, 300);
+
+      const adjusted = 50 / distance;
+      gravitationalDirection.setMag(adjusted);
+
+      return gravitationalDirection;
+    }
+
+    // Propotional to Distance
+    private getPropotionalToDistance(p: p5, v: p5.Vector) {
+      const gravitationalDirection = p5.Vector.sub(v, this.position);
+
+      let distance = gravitationalDirection.mag();
+      distance = p.constrain(distance, 100, 300);
+
+      const adjusted = (distance - 100) / 200;
+      gravitationalDirection.setMag(adjusted);
+
+      return gravitationalDirection;
     }
 
     update(p: p5) {
       let mouse = p.createVector(p.mouseX, p.mouseY);
 
-      // direction
-      this.acceleration = p5.Vector.sub(mouse, this.position);
-      this.acceleration.setMag(0.03);
-
+      // this.acceleration = this.getConstantAcceleration(p, mouse);
+      // this.acceleration = this.getInverseToDistance(p, mouse);
+      this.acceleration = this.getPropotionalToDistance(p, mouse);
       this.velocity.add(this.acceleration);
       this.velocity.limit(this.topSpeed);
       this.position.add(this.velocity);
     }
+
     show(p: p5) {
       p.stroke(0);
       p.fill(175);
