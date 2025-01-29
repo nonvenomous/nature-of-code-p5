5e24ac246f14cb60d09dbf6d2d5ca568df5b3af9
nonvenomous <rawlsriot@gmail.com>
2025-01-29 19:35:24 +0100

    Omit adapter config for vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter({
-      fallback: '200.html', // may differ from host to host
-    }),
+    adapter: adapter(),
   },
 };
