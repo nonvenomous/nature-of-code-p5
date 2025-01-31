5a2eb5e5afe201601d452dd95c64b9f3a4378bf0
nonvenomous <rawlsriot@gmail.com>
2025-01-31 13:20:27 +0100

    Add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 export const ssr = false;
 export const prerender = true;
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
